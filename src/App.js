f1f2813a1ad8c88909ff6df15baabdb3fee9e1ab
ewami <efili1989@gmail.com>
2020-06-16 19:19:23 +0200

    create navbar with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import HomePage from "./pages/HomePage";
 import CollectionsListPage from "./pages/CollectionsListPage";
 import CollectionPage from "./pages/CollectionPage";
 import BookPage from "./pages/BookPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div>
+      <Navigation />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/collectionslist/:id" component={CollectionsListPage} />
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+import NavbarItem from "./NavbarItem";
+import "./index.css";
+
+export default function Navigation() {
+  return (
+    <Navbar bg="light" expand="md">
+      <Navbar.Brand as={NavLink} to="/">
+        <p>BOOK APP</p>
+      </Navbar.Brand>
+      <Nav className="ml-auto">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav style={{ width: "100%" }} fill>
+            <NavbarItem path="/" linkText="Homepage" />
+            <NavbarItem
+              path="/collectionslist/:id"
+              linkText="Your collections"
+            />
+            <NavbarItem path="/login" linkText="Login" />
+          </Nav>
+        </Navbar.Collapse>
+      </Nav>
+    </Navbar>
+  );
+}
