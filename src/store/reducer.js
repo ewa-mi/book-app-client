9c655a4ccd2860acf95579f082931b59fcb6887c
ewami <efili1989@gmail.com>
2020-06-16 14:24:35 +0200

    set up store and root reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+// import someFeatureReducer from "./someFeature/reducer";
+
+const reducer = combineReducers({
+  // someFeature: someFeatureReducer, etc.
+});
+
+export default reducer;
