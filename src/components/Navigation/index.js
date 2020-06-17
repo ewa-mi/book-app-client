332355b7b61d3872919857ce2a055256c0dc3cec
ewami <efili1989@gmail.com>
2020-06-17 22:23:22 +0200

    add logo to navbar, css

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,13 +3,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
 import NavbarItem from "./NavbarItem";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 import "./index.css";
 
 export default function Navigation() {
   return (
     <Navbar bg="light" expand="md">
       <Navbar.Brand as={NavLink} to="/">
-        <p>BOOK APP</p>
+        <Logo className="logo" />
       </Navbar.Brand>
       <Nav className="ml-auto">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
