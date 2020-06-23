22d73896c88bab53685bd1e3118b19d09732dcac
ewami <efili1989@gmail.com>
2020-06-23 18:28:15 +0200

    implement redirection to Homepage when logout

--- src/components/Navigation/LoggedIn.js
+++ src/components/Navigation/LoggedIn.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { logOut } from "../../store/user/actions";
 import Button from "react-bootstrap/Button";
 import { selectUser } from "../../store/user/selectors";
@@ -7,11 +8,19 @@ import Nav from "react-bootstrap/Nav";
 
 export default function LoggedIn() {
   const dispatch = useDispatch();
+  const history = useHistory();
   const user = useSelector(selectUser);
   return (
     <>
       <Nav.Item style={{ padding: ".5rem 1rem" }}>{user.email}</Nav.Item>
-      <Button onClick={() => dispatch(logOut())}>Logout</Button>
+      <Button
+        onClick={() => {
+          dispatch(logOut());
+          history.push("/");
+        }}
+      >
+        Logout
+      </Button>
     </>
   );
 }
