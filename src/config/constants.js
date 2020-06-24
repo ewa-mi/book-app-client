cb03bee62b7dfd68915cce852d26ed99d7907d22
ewami <efili1989@gmail.com>
2020-06-24 22:37:28 +0200

    add host

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,5 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.API_URL ||
+  "http://localhost:4000" ||
+  "https://book-app-portfolio-project.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 5000;
