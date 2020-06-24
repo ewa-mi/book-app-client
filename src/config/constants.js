819c3879c1dc96da1d6139e754765eb369270ea3
ewami <efili1989@gmail.com>
2020-06-24 22:58:55 +0200

    update recent commit

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,2 @@
-export const apiUrl =
-  process.env.API_URL ||
-  "http://localhost:4000" ||
-  "https://book-app-portfolio-project.herokuapp.com";
+export const apiUrl = "https://book-app-portfolio-project.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 5000;
