b6b07f0b14b8b64db6c8444d49e30a54e49d74eb
ewami <efili1989@gmail.com>
2020-07-24 15:45:48 +0200

    test a thunk / mock function

--- src/store/bookPage/tests/actions.test.js
+++ src/store/bookPage/tests/actions.test.js
@@ -1,10 +1,10 @@
 import { SET_BOOK_DETAILS, setBookDetails, fetchBookDetails } from "../actions";
-// import { appLoading, appDoneLoading } from "../../appState/actions";
-// import axios from "axios";
+import { APP_LOADING, APP_DONE_LOADING } from "../../appState/actions";
+import axios from "axios";
 
-// jest.mock("axios");
+jest.mock("axios");
 
-describe("#fetchBookDetails", () => {
+describe("#setBookDetails", () => {
   describe("if given an object of bookDetails", () => {
     test("should return an action object", () => {
       // test data simulating bookDetails
@@ -53,6 +53,52 @@ describe("#fetchBookDetails", () => {
       const action = setBookDetails(bookDetails);
       // do assertion on function return
       expect(action.payload).toEqual(expected.payload);
+    });
+  });
+});
+
+// MOCKING FUNCTION
+
+describe("#fetchBookDetails", () => {
+  describe("when called", () => {
+    test("should dispatch an appLoading", async () => {
+      const responseData = { fakeBookDetails: {} };
+      axios.get.mockImplementationOnce(() => Promise.resolve(responseData));
+      const dispatch = jest.fn();
+      await fetchBookDetails("collectionId", "bookId")(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+      expect(dispatch).toHaveBeenCalledWith({
+        type: APP_LOADING,
+      });
+      expect(dispatch).toHaveBeenCalledTimes(3);
+    });
+
+    test("should dispatch a setBookDetails", async () => {
+      const responseData = { fakeBookDetails: {} };
+      axios.get.mockImplementationOnce(() => Promise.resolve(responseData));
+      const dispatch = jest.fn();
+      await fetchBookDetails("collectionId", "bookId")(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+      expect(dispatch).toHaveBeenCalledWith({
+        payload: undefined,
+        type: SET_BOOK_DETAILS,
+      });
+      expect(dispatch).toHaveBeenCalledTimes(3);
+    });
+
+    test("should dispatch an appDoneLoading", async () => {
+      const responseData = { fakeBookDetails: {} };
+      axios.get.mockImplementationOnce(() => Promise.resolve(responseData));
+      const dispatch = jest.fn();
+      await fetchBookDetails("collectionId", "bookId")(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+      expect(dispatch).toHaveBeenCalledWith({
+        type: APP_DONE_LOADING,
+      });
+      expect(dispatch).toHaveBeenCalledTimes(3);
     });
   });
 });
